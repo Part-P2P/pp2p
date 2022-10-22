148228fb59fab7598c2b78405b18b7c0794c2f10
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-10-22 21:04:58 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+class pp2p {
+  
+  constructor(server, id, clientid) {
+    this.server = server;
+    this.id = id;
+    this.clientId = clientid;
+  }
+  
+  ping() {
+    const start = Date.now();
+    var res = http_request(this.server);
+    const end = Date.now()
+    return (end - start);
+  }
+ 
+  isDominantClient(otherping, p2p) {
+    if (otherping > this.ping()) {
+      return false;
+      this.send(p2p, 'pp2p.tech:notDom');
+    } else {
+      this.send(p2p, 'pp2p.tech:Dom');
+      return true;
+    }
+  }
+  
