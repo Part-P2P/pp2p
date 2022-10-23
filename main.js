6c3bcc0a8921a1bc1082509aa641e2c941f3ec11
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-10-23 22:02:39 +0200

    Update main.js

--- main.js
+++ main.js
@@ -45,10 +45,10 @@ class PP2P {
   connect(id) {
     this.id = id;
     this.connection = this.peer.connect(this.id);
-    this.connection.on('open', function() {
-      this.log(1, 'TypeReference not null!');
+    this.connection.on('open', sendMessageTemp());
+    function sendMessageTemp() {
       this.connection.send({"scope":"pp2p", "do":"connection", "content":"NIL"});
-    });
+    }
     this.connection.on('data', function(data) {
       if (JSON.parse(data).scope == "pp2p" && JSON.parse(data).do == "connection" && JSON.parse(data).content == "DONE") {
         this.log(1, 'Connection enstabilished, now declaring dominant server!');
