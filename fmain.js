1410e4f1ce1a2fefa50d2c60acaf06d6d999aec5
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-10-24 15:09:27 +0200

    Update fmain.js

--- fmain.js
+++ fmain.js
@@ -98,6 +98,7 @@ const PP2P = {
           PP2P.log(1, 'Dominant, send to 2nd client a not-dominant confirm');
         }
         PP2P.connection.send({"scope":"pp2p", "do":"dominant", "content":PP2P.dominant});
+        break PP2P;
         return PP2P.connection;
       }
     });
