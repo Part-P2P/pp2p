bc0d6d672f720d80121a49b79cc73d47b05b4320
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-10-25 11:10:49 +0200

    Update main.js

--- main.js
+++ main.js
@@ -34,11 +34,11 @@ const PP2P = {
           
           if (get.content.type == 'GET') {
             fetch(get.content.url).then(response => { return response.text() }).then(data => {
-              PP2P.connection.send({"scope":"response", "content":{"requestId":get.requestId, "content":data}});
+              PP2P.connection.send({"scope":"response", "content":{"requestId":get.requestId, "response":data}});
             });
           } else if (data.content.type == 'POST') {
             fetch(data.content.url, {headers:get.content.headers, body:get.content.body}).then(response => { return response.text() }).then(data => {
-              PP2P.connection.send({"scope":"response", "content":{"requestId":get.requestId, "content":data}});
+              PP2P.connection.send({"scope":"response", "content":{"requestId":get.requestId, "response":data}});
             });
           } else {
             PP2P.log(2, 'Unexpected ServerConnectionType from remote request');
