5d1b58fa2585db0f7925b8c752c8a17162d125a8
Federico <61429263+FoxWorn3365@users.noreply.github.com>
2022-10-25 10:24:48 +0200

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,8 @@ const PP2P = {
           } else if (get.do == "connection") {
             PP2P.connection.send({"scope":"pp2p", "do":"connectionResponse", "content":"DONE"});
           } else if (get.do == "dominant") {
-            if (get.do.content) {
+            CommonJS.makeEvent(window, 'pp2pConnected', {'detail':get.content});
+            if (get.content) {
               PP2P.dominant = true;
             } else {
               PP2P.dominant = false;
@@ -112,10 +113,12 @@ const PP2P = {
         response = response.detail;
         
         if (PP2P.globalPing > response) {
+          CommonJS.makeEvent(window, 'pp2pConnected', {'detail':false});
           PP2P.dominant = true;
           PP2P.connection.send({"scope":"pp2p", "do":"dominant", "content":false});
           PP2P.log(1, 'This client is dominant, sending a non-dominant message to other peer');
         } else {
+          CommonJS.makeEvent(window, 'pp2pConnected', {'detail':false});
           PP2P.dominant = false;
           PP2P.connection.send({"scope":"pp2p", "do":"dominant", "content":true});
           PP2P.log(1, 'This client is not dominant, sending a dominant message to other peer');
